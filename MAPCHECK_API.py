0efe6c428a0351df1e65638941f49aec916a8049
souaix <souaix@gmail.com>
2023-10-20 17:34:09 +0800

    PRD&TEST

--- MAPCHECK_API.py
+++ MAPCHECK_API.py
@@ -38,33 +38,83 @@ eng_cim = cc.connect('CIM_ubuntu', 'mapcheck')
 
 app = Flask(__name__)
 
-@app.route('/mapcheck/', methods=['POST'],strict_slashes=False)
+@app.route('/mapcheck_prd/', methods=['POST'],strict_slashes=False)
 
-def mapcheck():
+def mapcheck_prd():
 
-    mo = request.get_json()[0]['mo']
+    # mo = request.get_json()[0]['mo']
+    lotno = request.get_json()[0]['lotno']
+    # lotno_ = lotno.replace('-','')
 
+    opnos=['S093_RW','S081','S081','S074_DS','S074_GD','S074_RW','S110_RW']
+    programid=['PROG00012','PROG00028','PROG00015','PROG00011','PROG00011','PROG00011','PROG00020']
+    BR=['EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO','EQP_AUTOPROGRAM_CO']
+    FILETYPE=['2ND_AVI','5S_MAP','TRACELOG','1ST_AVI','1ST_AVI','1ST_AVI','CUTRACELOG']
+
+    opno_str=''
+    for i in opnos:
+        opno_str = opno_str + "'"+str(i)+"',"
+    opno_str = opno_str[:-1]
+
+
+    #check EXEFORXML Y
     sql ='''
-        select BASELOTNO,BARCODE,FILE_TYPE,AVIMAP_FILENAME from
-        (SELECT PRODUCTNO,BASELOTNO FROM TBLWIPLOTBASIS WHERE MONO =\''''+mo+'''\') as WIPBS
+        select ATTR.SERIALNO,PRDOP.OPNO from
+        (SELECT PRODUCTNO,BASELOTNO FROM TBLWIPLOTBASIS WHERE BASELOTNO =\''''+lotno+'''\') as WIPBS
         INNER JOIN
-        (select * from TBLPRDOP where opno ='S093_RW') as PRDOP
+        (select * from TBLPRDOP where opno in ('''+opno_str+''') )as PRDOP
         ON WIPBS.PRODUCTNO=PRDOP.PRODUCTNO
         INNER JOIN
         (select * from TBLPRDOPATTRIB where ATTRIBNO ='EXEFORXML' and ATTRIBVALUE='Y') as ATTR
-        on PRDOP.serialno = ATTR.SERIALNO
-        INNER JOIN
-        (select * from TBLPRDOPATTRIB where attribno = 'EQP_AUTOPROGRAM_CO' AND ATTRIBVALUE ='PROG00012') AS ATTCO
-        ON PRDOP.SERIALNO=ATTCO.SERIALNO    
-        LEFT JOIN
-        (select * from OPENQUERY(ERP,'select * from MES.VIEW_AVIMAP_FILE WHERE MO_NO =\'\''''+mo+'''''\') ) AS avimap
-        ON WIPBS.BASELOTNO COLLATE Chinese_Taiwan_Stroke_CI_AS = avimap.LOTNO
+        on PRDOP.serialno = ATTR.SERIALNO        
         '''
+    
+    df_check_xml = pd.read_sql(sql,eng_mes)
 
-    df_PROG00012 = pd.read_sql(sql,eng_mes)
-    df_PROG00012.to_sql('PROG0012', eng_cim, if_exists='replace', index=False)
-    result = [{'Message':'OK','Qty':'None'}]
+    for i,v in enumerate(opnos):
+        df_check_xml_opno = df_check_xml[df_check_xml['OPNO']==v]
+        df_check_xml_opno.reset_index(drop=True,inplace=True)
+        #EXEFORXML=Y => check PROG00012
+        if(len(df_check_xml_opno)>0):        
+
+            serialno = df_check_xml_opno['SERIALNO'][0]
+
+            sql ="select * from TBLPRDOPATTRIB where attribno = '"+BR[i]+"' AND ATTRIBVALUE like '"+programid[i]+"%' AND SERIALNO ='"+serialno+"'"                    
+            df_PROGRAM = pd.read_sql(sql,eng_mes)     
+            #PROGRAM existed => check AVIMAP
+            if(len(df_PROGRAM)>0):
+                if(FILETYPE[i] in ['2ND_AVI','1ST_AVI']):
+                    sql="select * from OPENQUERY(RIS,'select * from MES.VIEW_AVIMAP_FILE WHERE LOTNO=''"+lotno+"'' AND FILE_TYPE=''"+FILETYPE[i]+"''')"
+
+                elif(FILETYPE[i] in ['5S_MAP']):
+                    sql="select * from OPENQUERY(RIS,'select * from MES.VIEW_5SMAP_FILE WHERE LOTNO=''"+lotno+"''')"
+
+                elif(FILETYPE[i] in ['TRACELOG']):
+                    sql="select * from OPENQUERY(RIS,'select * from MES.VIEW_TRACELOG_FILE WHERE LOTNO=''"+lotno+"''')"
+
+                elif(FILETYPE[i] in ['CUTRACELOG']):
+                    sql="select * from OPENQUERY(RIS,'select * from MES.VIEW_TRACELOG_FILE WHERE CU_LOG_FILENAME IS NOT NULL AND LOTNO=''"+lotno+"''')"
+
+                df_MAPCHECK = pd.read_sql(sql,eng_mes)         
+                #AVIMAP => return ok
+                if(len(df_MAPCHECK)>0):
+                    result = [{'Result':'PASS','Message':programid[i]+' PASS'}]    
+                                
+                #else => return MAPCHECK fail
+                else:
+                    result = [{'Result':'FAIL','Message':lotno+' in '+v+': cannot find '+FILETYPE[i]+', please check'}]
+                    break
+                    #PROGRAM is not existed
+            else:
+                result = [{'Result':'PASS','Message':'its have no '+programid[i]+' rule'}]
+
+        #EXEFORXML<>Y => don't need check        
+        else:
+            result = [{'Result':'PASS','Message':v+',its have no EXEFORXML rule'}]
+            # break
+
     return jsonify(result)
+
 
 server_mc = WSGIServer(('10.21.98.21',7777),app,log=None)
 server_mc.start()
